ddf86820ef8c4622d002c2a42e3bbcef8cce3fec
hossein_mhm <hosseinsoori1998@yahoo.com>
2020-06-27 18:55:09 +0430

    send

---
+++ app/src/main/java/com/javapapers/todo/login_activity.java
@@ -0,0 +1,14 @@
+package com.javapapers.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
